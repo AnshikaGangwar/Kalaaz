edddd8ff6f23e46cf730b7aa3a0f5e49a433b104
Naman Patel <46571853+np453@users.noreply.github.com>
2021-03-06 09:54:08 +0530

    minor

--- client/src/components/showcase.jsx
+++ client/src/components/showcase.jsx
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import Footer from '../common/footer';
 import Navbar from './navbar';
 import temp from '../assets/temp.jpg';
-import Np from '../assets/np.jpg'
+// import Np from '../assets/np.jpg'
 
 class Showcase extends Component {
+    navlinks=[{
+        link_name:"ArtShow",
+        link_page:"/showcase" },
+    {
+        link_name:"Profile",
+        link_page:"/profile"
+    }];
     render() {
         return (
             <div className="showcase-wrapper">
-                <Navbar navlink1="feed" navlink2="Logout" />
+                <Navbar navlinks={this.navlinks}/>
                 <div className="frame-wrapper">
                     <div class="slideshow">
                     <div class="content">
